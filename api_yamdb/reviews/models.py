18e2f418ec9d15811b4b7eeec7e0a5fe19f78d84
Mihail Podzorov <mih.podzorov@gmail.com>
2024-07-15 21:42:14 +0300

    Создал модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Genres(models.Model):  # жанры произведений
+    name = models.CharField(max_length=256, )
+    slug = models.SlugField(max_length=50, )
+
+
+class Categories(models.Model):  # категории
+    name = models.CharField(max_length=256, )
+    slug = models.SlugField(max_length=50, )
+
+
+class Titles(models.Model):  # произведения
+    name = models.CharField(max_length=256, filter='name')
+    year = models.IntegerField(filter='year')
+    description = models.TextField()
+    genre = models.ForeignKey(Genres, filter='slug', )
+    category = models.OneToOneField(Categories, filter='slug', )
+
+    def __str__(self):
+        return self.name
+
+
+class Reviews(models.Model):  # отзывы на произведения
+    title_id = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='titles'
+    )
+
+
+class Comments(models.Model):  # комментарии к отзывам
+    title_id = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
