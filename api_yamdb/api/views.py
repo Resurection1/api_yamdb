b929fbcbfaf8e8389024f9bfcdad8b26d5f7f1d1
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2024-07-16 21:43:14 +0300

    Add views.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,87 @@
-from rest_framework import viewsets, mixins, permissions, filters
+from rest_framework import viewsets, mixins, permissions, filters, status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+from django.core.exceptions import ValidationError
+
+from django.db.models import QuerySet
 
 from django.shortcuts import get_object_or_404
 
+from reviews.models import Comment, Review, Category, Genre, Title
+
+from .serializers import (
+    CommentSerializer,
+    ReviewSerializer,
+    CategorySerializer,
+    GengreSerializer,
+    TitleSerializer,
+)
+
+from .permissions import IsAuthorOrAdminOrModerOnly, IsAdminOrReadOnly
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    ...
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrAdminOrModerOnly,
+    )
+    pagination_class = LimitOffsetPagination
+    http_method_names = ('get', 'post', 'patch', 'delete')
+
+    def perform_create(self, serializer) -> Response:
+        review_id: int = self.kwargs['review_id']
+        review: Review = get_object_or_404(Review, pk=review_id)
+        if serializer.is_valid():
+            serializer.save(author=self.request.user, review=review)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self) -> QuerySet:
+        review_id: int = self.kwargs['review_id']
+        review: Review = get_object_or_404(Review, pk=review_id)
+        comment_list: QuerySet[Comment] = Comment.objects.filter(review=review)
+        return comment_list
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    ...
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrAdminOrModerOnly,
+    )
+    pagination_class = LimitOffsetPagination
+    http_method_names = ('get', 'post', 'patch', 'delete')
+
+    def perform_create(self, serializer) -> Response:
+        title_id: int = self.kwargs['title_id']
+        title: Title = get_object_or_404(Title, pk=title_id)
+        user_reviews: QuerySet[Review] = Review.objects.filter(
+            title=title,
+            author=self.request.user
+        )
+        if user_reviews.exists():
+            raise ValidationError
+        serializer.save(author=self.request.user, title=title)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_queryset(self) -> QuerySet:
+        title_id: int = self.kwargs['title_id']
+        title: Title = get_object_or_404(Title, pk=title_id)
+        review_list: QuerySet[Review] = Review.objects.filter(title=title)
+        return review_list
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('genre__slug', 'category__slug', '=year', '=name',)
+    http_method_names = ('get', 'post', 'patch', 'delete')
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -16,8 +89,4 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class GenreViewSet(viewsets.ModelViewSet):
-    ...
-
-
-class TitleViewSet(viewsets.ModelViewSet):
     ...
