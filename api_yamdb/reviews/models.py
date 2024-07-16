fcdcbab1052a248de103c299af9e273d8f3dde53
Mihail Podzorov <mih.podzorov@gmail.com>
2024-07-16 22:31:21 +0300

    Исправление моделей

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,27 +8,29 @@ Author = get_user_model()
 
 class Genres(models.Model):  # жанры произведений
     name = models.CharField(max_length=256, )
-    slug = models.SlugField(max_length=50, )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Categories(models.Model):  # категории
     name = models.CharField(max_length=256, )
-    slug = models.SlugField(max_length=50, )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Titles(models.Model):  # произведения
     name = models.CharField(max_length=256,)
     year = models.IntegerField()
     description = models.TextField()
-    genre = models.ForeignKey(
-        Genres,
-        on_delete=models.CASCADE,
-        related_name='genres'
-    )
+    genre = models.ManyToManyField(Genres)
     category = models.OneToOneField(
         Categories,
         on_delete=models.CASCADE,
-        related_name='categories'
+        related_name='titles'
     )
 
     def __str__(self):
@@ -52,6 +54,11 @@ class Reviews(models.Model):  # отзывы на произведения
         verbose_name='Дата отзыва',
         auto_now_add=True
     )
+    titles = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
 
 
 class Comments(models.Model):  # комментарии к отзывам
@@ -64,4 +71,9 @@ class Comments(models.Model):  # комментарии к отзывам
     pub_date = models.DateTimeField(
         verbose_name='Дата комментария',
         auto_now_add=True
+    )
+    review = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name='comments'
     )
