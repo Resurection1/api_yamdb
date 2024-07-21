dbf7a9db01651dfbb289a6f39dc48e3694953fad
Podzorov Mihail <148565612+Resurection1@users.noreply.github.com>
2024-07-21 11:57:33 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,9 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework_simplejwt',
-
+    'django_filters',
 
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
